8da1cb911f42caf171ea3059bc553580fb08d16c
GlickGlickGlick <samarlimitedliabilitycompanyco@gmail.com>
2025-04-15 08:37:56 -0600

    added additional request methods to test

--- locust.py
+++ locust.py
@@ -2,20 +2,40 @@ from locust import HttpUser, task, between
 
 
 class APIUser(HttpUser):
-    # Set the base URL for all requests
     host = "https://jsonplaceholder.typicode.com"
-
-    wait_time = between(1, 5)  # Simulate wait time between requests
+    wait_time = between(1, 5)
 
     @task
     def get_posts(self):
-        self.client.get("/posts")  # This will now resolve to https://jsonplaceholder.typicode.com/posts
+        self.client.get("/posts")
 
     @task
     def get_users(self):
-        self.client.get("/users")  # This will now resolve to https://jsonplaceholder.typicode.com/users
+        self.client.get("/users")
+
+    @task
+    def update_post_put(self):
+        self.client.put("/posts/1", json={
+            "id": 1,
+            "title": "Updated Title",
+            "body": "Updated Body",
+            "userId": 1
+        })
+
+    @task
+    def update_post_patch(self):
+        self.client.patch("/posts/1", json={
+            "title": "Partially Updated Title"
+        })
+
+    @task
+    def delete_post(self):
+        self.client.delete("/posts/1")
 
     @task
     def create_post(self):
-        # This will now resolve to https://jsonplaceholder.typicode.com/posts
-        self.client.post("/posts", json={"title": "Test", "body": "Test Body", "userId": 1})
+        self.client.post("/posts", json={
+            "title": "Test",
+            "body": "Test Body",
+            "userId": 1
+        })
